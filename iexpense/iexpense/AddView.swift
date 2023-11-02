806a5942c6f55a38b1356be4e02d721f42232eff
Zabdiel Villalobos <zabdielv@ad.unc.edu>
2023-11-02 17:01:33 +0000

    Setting up GitHub Classroom Feedback

---
+++ iexpense/iexpense/AddView.swift
@@ -0,0 +1,41 @@
+//
+//  AddView.swift
+//  iexpense
+//
+//  Created by Zabdiel Villalobos on 11/2/23.
+//
+// Finally created a different view instead of chucking everything into one file
+import SwiftUI
+struct AddView: View {
+    @Environment (\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    var expenses: Expenses
+    let types = ["Business", "Personal"]
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+            }
+            .navigationTitle("Add New Expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+#Preview {
+    AddView(expenses: Expenses ())
+}
---
+++ iexpense/iexpense/iexpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iexpenseApp.swift
+//  iexpense
+//
+//  Created by Zabdiel Villalobos on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct iexpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
